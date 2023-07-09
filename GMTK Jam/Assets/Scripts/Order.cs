c7c5fc422962ff95a637f53be2a004e3d26d518d
giovannafb <you@example.com>
2023-07-09 09:04:38 -0300

    Pedido pronto

---
+++ GMTK Jam/Assets/Scripts/Order.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Order", menuName = "Order")]
+public class Order : ScriptableObject
+{
+    public Sprite Food;
+}
--- GMTK Jam/Assets/Scripts/OrderDisplay.cs
+++ GMTK Jam/Assets/Scripts/OrderDisplay.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class OrderDisplay : MonoBehaviour
 {
-    public Orders order;
-    public Text nameText;
-    public Text qtd;
-    public Image typeFood;
+    public Order _order;
+
+    
+
+    [SerializeField] TextMeshProUGUI qtdFood;
+
+    public Image foodImage;
 
     void Start()
     {
-        order = GetComponent<Orders>();
-        order.foodID = Random.Range(0, 6);
-        order.quantity = Random.Range(0, 6);
-        Debug.Log(order.name);
+        int _random = Random.Range(1,6);
+        foodImage.sprite = _order.Food;
+        qtdFood.text = _random.ToString();
     }
 }
